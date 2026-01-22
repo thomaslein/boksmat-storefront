62096826167f66f96233849911b913ddb2f7ae36
Thomas Lein <thomas.lein@gmail.com>
2026-01-22 23:02:56 +0100

    add cursor pointer to buttons

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -8,7 +8,7 @@ export default function BackButton() {
   return (
     <button
       onClick={() => router.back()}
-      className="mb-4 flex items-center text-sm font-sans text-neutral-500 hover:text-black transition-colors"
+      className="mb-4 flex items-center text-sm font-sans text-neutral-500 hover:text-black transition-colors cursor-pointer"
     >
       <span className="mr-2">←</span> Tilbake
     </button>
